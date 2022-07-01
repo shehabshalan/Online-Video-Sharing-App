1be0d2a12710948249aa49fc7d68c6eb8c6e0413
Shehab Shalan <shehab.shalan@hotmail.com>
2022-07-01 23:10:48 +0200

    route to pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "./App.css";
 import { Route, Switch } from "react-router-dom";
-import VideoList from "./components/VideoList";
-import VideoPage from "./components/VideoPage";
-import NotFound from "./components/NotFound";
-import Login from "./components/Login";
-import Register from "./components/Register";
-import VideoUpload from "./components/VideoUpload";
+import NotFound from "./pages/NotFound";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 import ProtectedRoutes from "./ProtectedRoutes";
+import Home from "./pages/Home";
+import Upload from "./pages/Upload";
+import VideoDetails from "./pages/VideoDetails";
 
 function App() {
   return (
@@ -15,9 +15,9 @@ function App() {
       <Switch>
         <Route path="/login" component={Login} />
         <Route path="/register" component={Register} />
-        <ProtectedRoutes exact path="/" component={VideoList} />
-        <ProtectedRoutes path="/video/:id" component={VideoPage} />
-        <ProtectedRoutes path="/upload" component={VideoUpload} />
+        <ProtectedRoutes exact path="/" component={Home} />
+        <ProtectedRoutes path="/video/:id" component={VideoDetails} />
+        <ProtectedRoutes path="/upload" component={Upload} />
         <Route path="*" component={NotFound} />
       </Switch>
     </div>
